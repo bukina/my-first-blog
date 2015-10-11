fd1d7cadfd87ca553f44963953b3966d83290dd9
Dariia <admin@admin.com>
2015-10-11 14:52:33 +0300

    Commit_1_dj_blog

---
+++ dj.py
@@ -0,0 +1,14 @@
+# def dj():
+#     a = 10
+#     if a > 5:
+#         print('a>5')
+#     elif a == 5:
+#         print('a=5')
+#     else:
+#         print('a<5')
+
+# dj()
+
+l = [1, 2, 3, 4, 5, 6]
+for item in l:
+    print(item)
